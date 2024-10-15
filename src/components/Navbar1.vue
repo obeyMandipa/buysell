270793e9b4becca6c1ec13ea5c6f601c056def3b
Rufaro O Mandipa <mandipaobey@gmail.com>
2024-10-16 01:50:23 +0200

    no comment

--- src/components/Navbar1.vue
+++ src/components/Navbar1.vue
@@ -2,16 +2,16 @@
 <template>
   <div>
     <div class=" flex justify-between items-center h-[70px]">
-      <div class="">BuySell</div>
+      <div class=" text-[22px] font-bold">BuySell</div>
       <div class="">
         <nav class="flex gap-4">
-          <a class="link link-hover">About us</a>
-          <a class="link link-hover">Deliveries</a>
+          <a class="link link-hover text-[20px]">About us</a>
+          <a class="link link-hover text-[20px]">Deliveries</a>
         </nav> 
       </div>
       <div class="space-x-[20px]">
-        <button class="btn">Sign in</button>
-        <button class="btn">Sign up </button>
+        <button class="btn w-[130px] bg-transparent border-black text-[17px]">Sign in</button>
+        <button class="btn w-[130px] bg-black border-black text-[17px] text-white">Sign up </button>
       </div>
     </div>
   </div>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -2,14 +2,26 @@
 <template>
   <div>
     <header>
-      <div class="bg-blue-300 h-[60vh]"> 
+      <div class="bg-gradient-to-r from-violet-300 h-[60vh]"> 
         <Navbar1></Navbar1>
+
+        <div class="flex">
+          <p class="text-[70px] font-bold  w-[1000px] ml-[70px] mt-[60px]">
+            Getting the best experience in buying, selling and exploring...
+          </p>
+
+          <div class="flex ">
+            <img src="https://i.pinimg.com/564x/f6/09/41/f6094121ae8689a84eebb990d664440a.jpg" alt="" class="h-[600px] rounded-[350px] mt-[100px] ml-[60px]">
+            <img src="https://i.pinimg.com/736x/a0/7f/21/a07f2141f649d6122dd1db4d5863b95f.jpg" alt="" class="absolute h-[250px] w-[250px] rounded-full ml-[570px] mt-[70px]">
+            <img src="https://i.pinimg.com/564x/40/0f/02/400f0222ff7bd0442c68e4a27fbd1b3a.jpg" alt="" class="absolute h-[300px] w-[320px] rounded-full ml-[-200px] mt-[250px]"
+          </div>
+        </div>
       </div>
     </header>
 
-    <main class=" mt-[200px]">
+    <main class=" mt-[30vh]">
       <div class=" space-y-11">
-        <div class="explore bg-violet-300 h-[40vh] content-center">
+        <div class="explore bg-violet-300 h-[90vh] justify-center">
         <h1>Explore</h1>
         </div>
         <div class="sell bg-violet-300 h-[40vh]">
