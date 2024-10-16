3c3f33c736522643a64aca222944ee5b27fdedec
Rufaro O Mandipa <mandipaobey@gmail.com>
2024-10-17 01:27:14 +0200

    finished creating the landing page

--- src/components/Navbar1.vue
+++ src/components/Navbar1.vue
@@ -2,16 +2,16 @@
 <template>
   <div>
     <div class=" flex justify-between items-center h-[70px]">
-      <div class=" text-[22px] font-bold">BuySell</div>
+      <div class=" text-[22px] font-bold">image and logo</div>
       <div class="">
         <nav class="flex gap-4">
-          <a class="link link-hover text-[20px]">About us</a>
-          <a class="link link-hover text-[20px]">Deliveries</a>
+          <a class="link link-hover hover:underline-offset-8 text-[20px] font-bold hover:text-gray-500 duration-300 hover:-translate-y-1 ">About us</a>
+          <a class="link link-hover hover:underline-offset-8 text-[20px] font-bold hover:text-gray-500 duration-300 hover:-translate-y-1 ">Deliveries</a>
         </nav> 
       </div>
       <div class="space-x-[20px]">
-        <button class="btn w-[130px] bg-transparent border-black text-[17px]">Sign in</button>
-        <button class="btn w-[130px] bg-black border-black text-[17px] text-white">Sign up </button>
+        <button class="btn w-[130px] text-[17px] transition ease-in-out delay-150 bg-transparent hover:-translate-y-1 hover:scale-110 border-2 border-black hover:bg-transparent duration-300 ..."><RouterLink to="/shop"</RouterLink></button>
+        <button class="btn w-[130px] text-[17px] transition ease-in-out delay-150 bg-black hover:-translate-y-1 hover:scale-110 hover:bg-black border-2 border-black duration-300 ... text-white">Sign up </button>
       </div>
     </div>
   </div>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import PrimeVue from "primevue/config";
 import Aura from '@primevue/themes/aura';
 
 
+
 const app = createApp(App)
 
 app.use(PrimeVue, {
@@ -15,7 +16,8 @@ app.use(PrimeVue, {
         preset: Aura
     }
 });
-
+    
 app.use(router)
+
 
 app.mount('#app')
