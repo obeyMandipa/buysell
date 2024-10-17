96a10093884b0f22bf9c44737fc8a0b5e5bc238a
Rufaro O Mandipa <mandipaobey@gmail.com>
2024-10-17 16:02:30 +0200

    added a modal element but et to be edited

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -8,24 +8,37 @@
     <div class="h-[50vh] bg-violet-300">
 
     </div>
-    <div class="mt-[40px] pl-[90px] pr-[90px] bg-red-200">
+    <div class="mt-[40px] pl-[90px] pr-[90px]">
       <p class="text-[30px] font-bold ">Prodcuts for you...</p>
 
       <div class="products grid grid-cols-4 gap-2 mt-[40px]">
-        <div class="card card-compact bg-base-100 w-[400px] h-[400px] shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+        <div onclick="my_modal_3.showModal()" class="card  card-compact bg-base-100 w-[400px] h-[400px] shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
           <figure>
             <img
               src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
               alt="Shoes" />
           </figure>
           <div class="card-body">
-            <h2 class="card-title">Shoes!</h2>
-            <p >$50</p>
+            <span class="flex space-x-[230px]">
+              <h2 class="card-title">Shoes</h2>
+              <p class="text-[20px] font-bold mt-[5px]">$50.00</p>
+            </span>
+            <p>Product short decrition can be optional...</p>
             <div class="card-actions justify-end">
               <button class="btn rounded-[30px] bg-transparent border-2 border-black transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-black hover:text-white duration-300">Add to cart</button>
             </div>
           </div>
         </div>
+
+        <dialog id="my_modal_3" class="modal">
+          <div class="modal-box">
+            <form method="dialog">
+              <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            </form>
+            <h3 class="text-lg font-bold">Hello!</h3>
+            <p class="py-4">Press ESC key or click on ✕ button to close</p>
+          </div>
+        </dialog>
 
         <div class="card card-compact bg-base-100 w-[400px] h-[400px] shadow-xl">
           <figure>
