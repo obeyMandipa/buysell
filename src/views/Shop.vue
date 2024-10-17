b9732879c6072bad7b7a78989ad987863719f303
Rufaro O Mandipa <mandipaobey@gmail.com>
2024-10-18 01:05:40 +0200

    completed the modal for the product description

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -37,31 +37,49 @@
             <div class="modal-action">
               <form method="dialog">
                 <!-- if there is a button, it will close the modal -->
-                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                <button class="btn btn-sm btn-circle btn-ghost absolute text-white font-bolder bg-red-500 hover:bg-red-500 right-2 top-2">✕</button>
               </form>
               <div class="flex space-x-[140px]">
-                <img src="" alt="" class=" bg-red-200 h-[600px] w-[600px]">
-                <div class="details bg-red-200 h-[700px] w-[750px] ">
+                <img src="https://i.pinimg.com/736x/53/67/56/5367560231aca4739af4fe5e8f88fa71.jpg" alt="" class=" shadow-2xl h-[600px] w-[600px] rounded-[20px]">
+                <div class="details h-[700px] w-[750px] ">
                   <p class="text-[30px] font-bold">Prodcut name</p>
                   <p class="">Product discription</p>
                   <!-- product rating -->
-                  <div class="rating">
-                    <input type="radio" name="rating-1" class="mask mask-star" />
-                    <input type="radio" name="rating-1" class="mask mask-star" checked="checked" />
-                    <input type="radio" name="rating-1" class="mask mask-star" />
-                    <input type="radio" name="rating-1" class="mask mask-star" />
-                    <input type="radio" name="rating-1" class="mask mask-star" />
+                  <div class="rating mt-[10px]">
+                    <input type="radio" name="rating-1" class="mask mask-star bg-green-600" />
+                    <input type="radio" name="rating-1" class="mask mask-star bg-green-600" checked="checked" />
+                    <input type="radio" name="rating-1" class="mask mask-star bg-green-600" />
+                    <input type="radio" name="rating-1" class="mask mask-star bg-green-600" />
+                    <input type="radio" name="rating-1" class="mask mask-star bg-green-600" />
                   </div>
-                  <p class="text-[30px] font-bold">$300.00</p>
-                  <h1 class="text-[20px] font-bold">Quantity in stock</h1>
+                  <p class="text-[30px] font-bold mt-[30px]">$300.00</p>
+                  <h1 class="text-[20px] font-bold mt-[30px]">Quantity in stock</h1>
                   <p class="text-[20px]">30</p>
-                  <div class="flex space-x-4">
-                    <div class="flex space-x-4 w-[200px] justify-center h-[50px] border rounded-full p-2">
+                  <div class="flex space-x-4 mt-[30px]">
+                    <div class="flex space-x-4 w-[200px] justify-center h-[50px] bg-gray-100 rounded-full p-2 ">
                       <button class=" h-[27px] w-[27px] text-[24px] font-bold" @click="reduceUnit">-</button>
-                      <input v-model="unit" type="text" class=" h-[27px] w-[35px] p-2 text-center" placeholder="1" readonly>
+                      <input v-model="unit" type="text" class=" h-[27px] w-[35px] p-2 text-center bg-gray-100 mt-[3px]" placeholder="1" readonly>
                       <button class=" h-[27px] w-[27px] text-[22px] font-bold" @click="addUnit">+</button>
                     </div>
-                    <p class="">Adjust your order quantity</p>
+                    <p class="mt-[10px]">Adjust your order quantity</p>
+                  </div>
+                  <div class="flex space-x-4 mt-[30px]">
+                    <button class="btn w-[200px] rounded-[30px] bg-transparent border-2 border-black transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-black text-[16px] hover:text-white duration-300">Buy</button>
+                    <button class="btn w-[200px] rounded-[30px] bg-transparent border-2 border-black transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-black text-[16px] hover:text-white duration-300">Add to cart</button>
+                  </div>
+                  <div class=" mt-[40px] border-t-2 p-2 border-gray-500 flex justify-between">
+                    <div class="">
+                      <p class="flex gap-2 text-[15px] font-bold">
+                        <svg class="h-8 w-8 text-orange-500 mt-[-5px]"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="7" cy="17" r="2" />  <circle cx="17" cy="17" r="2" />  <path d="M5 17h-2v-4m-1 -8h11v12m-4 0h6m4 0h2v-6h-8m0 -5h5l3 5" />  <line x1="3" y1="9" x2="7" y2="9" /></svg>
+                        Free delivery
+                      </p>
+                      <p class="text-[13px]">The product will safely be delivered to the stipulated location</p>
+                    </div>
+                    <div class="form-control mt-[20px]">
+                      <label class="label cursor-pointer">
+                        <input type="checkbox" checked="checked" class="checkbox checkbox-success" />
+                      </label>
+                    </div>
                   </div>
                 </div>
               </div>
